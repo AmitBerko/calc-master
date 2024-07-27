f86a5d59c54baab6c9115011a9818e7f34a830cc
amitberko <amit.berko10@gmail.com>
2024-07-27 14:59:23 +0300

    Almost finished authentication

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,41 +1,62 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import User from '../models/UserModel.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
-
+const saltRounds = 10
 let refreshTokens = [] // Change to mongo or redis later
+
 router.post('/register', async (req, res) => {
 	const { email, username, password } = req.body
 
 	// Create a user in the db
 	try {
-		const newUser = new User({ email, username, hashedPassword: password })
+		const hashedPassword = await bcrypt.hash(password, saltRounds)
+		const newUser = new User({ email, username, hashedPassword })
 		await newUser.save()
 
-    console.log(newUser)
+		const userObject = newUser.toObject()
+		delete userObject.hashedPassword // Rather not send it to the frontend
 
-		const { accessToken, refreshToken } = generateTokensAndSetCookie(email, res)
+		const { accessToken, refreshToken } = generateTokensAndSetCookie(userObject._id, res)
 		refreshTokens.push({ email, refreshToken })
-		res.status(200).json(accessToken)
+		res.status(200).json({ accessToken, user: userObject })
 	} catch (error) {
 		console.log(`error is ${error}`)
 		if (error.code === 11000) {
-      const field = Object.keys(error.keyValue)[0]
-      console.log(Object.keys(error.keyValue))
+			const field = Object.keys(error.keyValue)[0]
+			console.log(Object.keys(error.keyValue))
 			return res.status(409).json(`${field} already exists`)
 		}
 
-    res.status(400).json(error)
+		res.status(400).json(error)
 	}
 })
 
 router.post('/login', async (req, res) => {
 	const { email, password } = req.body
+	try {
+		const user = await User.findOne({ email })
+		if (!user) {
+			return res.status(401).json({ error: 'Invalid email or password' })
+		}
 
-	const { accessToken, refreshToken } = generateTokensAndSetCookie(email, res)
-	refreshTokens.push({ email, refreshToken })
-	res.json(accessToken)
+		const doesPasswordMatch = await bcrypt.compare(password, user.hashedPassword)
+		if (!doesPasswordMatch) {
+			return res.status(401).json({ error: 'Invalid email or password' })
+		}
+
+		const { accessToken, refreshToken } = generateTokensAndSetCookie(user._id, res)
+		const userObject = user.toObject()
+		delete userObject.hashedPassword
+
+		refreshTokens.push({ email, refreshToken })
+		res.json({ accessToken, user: userObject })
+	} catch (error) {
+		console.log(`error is ${error}`)
+		res.status(500).json({ error: 'Internal server error' })
+	}
 })
 
 router.get('/refreshTokens', async (req, res) => {
@@ -65,15 +86,13 @@ router.post('/logout', async (req, res) => {
 
 router.get('/refresh-access-token', async (req, res) => {
 	const refreshToken = req.cookies.refreshToken
-	// console.log(`token is ${refreshToken}`)
 	if (!refreshToken) {
 		return res.status(400).json({ error: 'Refresh token was not found' })
 	}
 	try {
 		const user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
-
-		const newAccessToken = jwt.sign({ email: user.email }, process.env.ACCESS_TOKEN_SECRET, {
-			expiresIn: '15s',
+		const newAccessToken = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN_SECRET, {
+			expiresIn: '30m',
 		})
 
 		res.status(200).json(newAccessToken)
@@ -84,10 +103,22 @@ router.get('/refresh-access-token', async (req, res) => {
 
 router.post('/getUser', authenticateToken, async (req, res) => {
 	try {
-    console.log('user is', req.user)
+		console.log('user is', req.user)
 		res.status(200).json(req.user)
 	} catch (error) {
 		res.status(401).json(error)
+	}
+})
+
+router.post('/updateUser', authenticateToken, async (req, res) => {
+	console.log(req.user)
+	const updatedValues = req.body
+	console.log(updatedValues)
+	try {
+		const newUser = await User.findByIdAndUpdate(req.user._id, updatedValues, { new: true })
+		res.status(200).json(newUser)
+	} catch (error) {
+		res.status(401).json({ error: 'Unauthorized' })
 	}
 })
 
@@ -99,16 +130,22 @@ function authenticateToken(req, res, next) {
 
 	if (!token) return res.status(401).json({ error: "Access token doesn't exist" })
 
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
 		if (err) return res.status(401).json({ error: 'Unauthorized' })
-		req.user = user
+
+		console.log(`the user inside authenticatin is `, user)
+		req.user = await User.findById(user._id).select('-hashedPassword')
 		next()
 	})
 }
 
-function generateTokensAndSetCookie(email, res) {
-	const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' })
-	const refreshToken = jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+function generateTokensAndSetCookie(userId, res) {
+	const accessToken = jwt.sign({ _id: userId }, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: '30m',
+	})
+	const refreshToken = jwt.sign({ _id: userId }, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: '7d',
+	})
 
 	res.cookie('refreshToken', refreshToken, {
 		httpOnly: true,
