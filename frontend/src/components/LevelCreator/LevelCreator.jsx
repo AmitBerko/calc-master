7661cd64172c894c8af93dc0eb75ac61a7180c0c
amitberko <amit.berko10@gmail.com>
2024-07-28 21:57:43 +0300

    Started to work on the `LevelCreator` component

---
+++ frontend/src/components/LevelCreator/LevelCreator.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function LevelCreator() {
+  return (
+    <div className="calculator-container">
+      <div className="screen-container">
+        <div className="screen-header">
+          <div className="moves">3</div>
+          <div className="goal">321</div>
+        </div>
+        <div className="screen-footer">123</div>
+      </div>
+      <div className="buttons-container"></div>
+    </div>
+  )
+}
+
+export default LevelCreator
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
+import { Box, Toolbar } from '@mui/material'
 import HomepageDrawer from '../components/HomepageDrawer'
+import HomepageAppBar from '../components/HomepageAppBar'
+import LevelCreator from '../components/LevelCreator/LevelCreator'
 
 const drawerWidth = 190
 
 function Homepage() {
 	const [mobileOpen, setMobileOpen] = useState(false)
 	const [isClosing, setIsClosing] = useState(false)
+	const [selectedComponent, setSelectedComponent] = useState('levelCreator')
 
 	const handleDrawerToggle = () => {
 		if (!isClosing) {
@@ -17,64 +19,35 @@ function Homepage() {
 
 	return (
 		<Box sx={{ display: 'flex' }}>
-			<AppBar
-				position="fixed"
-				sx={{
-					width: { sm: `calc(100% - ${drawerWidth}px)` },
-					ml: { sm: `${drawerWidth}px` },
-				}}
-			>
-				<Toolbar>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						edge="start"
-						onClick={handleDrawerToggle}
-						sx={{ mr: 2, display: { sm: 'none' } }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" noWrap component="div">
-						Responsive drawer
-					</Typography>
-				</Toolbar>
-			</AppBar>
-      
+			<HomepageAppBar handleDrawerToggle={handleDrawerToggle} />
+
 			<HomepageDrawer
 				mobileOpen={mobileOpen}
 				setMobileOpen={setMobileOpen}
-				isClosing={isClosing}
 				setIsClosing={setIsClosing}
 			/>
 
+			{/* <Box
+				component="main"
+				sx={{
+					flexGrow: 1,
+					p: 3,
+					width: { xs: '100%', sm: `calc(100% - ${drawerWidth}px)` },
+					display: 'flex',
+					justifyContent: 'center',
+				}}
+			> */}
 			<Box
 				component="main"
-				sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+				sx={{
+					marginTop: { xs: '4.75rem', sm: '5.25rem' },
+					marginLeft: { lg: '1.25rem' },
+					display: 'flex',
+					justifyContent: { xs: 'center', lg: 'start' },
+					width: { xs: '100%', sm: `calc(100% - ${drawerWidth}px)` },
+				}}
 			>
-				<Toolbar />
-				<Typography paragraph>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-					facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-					gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-					donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-					adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-					Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-					imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-					arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-					donec massa sapien faucibus et molestie ac.
-				</Typography>
-				<Typography paragraph>
-					Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-					facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-					tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-					consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-					vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-					hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-					tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-					nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-					accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-				</Typography>
+				{selectedComponent === 'levelCreator' && <LevelCreator />}
 			</Box>
 		</Box>
 	)
