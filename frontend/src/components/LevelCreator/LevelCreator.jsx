07c2a81328e75234de4f100cb6eefce6d72677df
amitberko <amit.berko10@gmail.com>
2024-07-31 23:36:19 +0300

    Started to work on the calculator's design

--- frontend/src/components/LevelCreator/LevelCreator.jsx
+++ frontend/src/components/LevelCreator/LevelCreator.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 
 function LevelCreator() {
-  return (
-    <div className="calculator-container">
-      <div className="screen-container">
-        <div className="screen-header">
-          <div className="moves">3</div>
-          <div className="goal">321</div>
-        </div>
-        <div className="screen-footer">123</div>
-      </div>
-      <div className="buttons-container"></div>
-    </div>
-  )
+	return (
+		<div className="calculator-container">
+			<div className="screen-container">
+				<div className="screen-content">
+					<div className="level-info">
+						<div className="moves">MOVES: 4</div>
+						<div className="goal">GOAL: 1234</div>
+					</div>
+					<div className="result-container">
+						<div className="result">12345</div>
+					</div>
+				</div>
+			</div>
+			<div className="buttons-container">asdas</div>
+		</div>
+	)
 }
 
 export default LevelCreator
