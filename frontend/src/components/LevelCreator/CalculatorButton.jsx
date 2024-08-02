9c776ea14fda4e293e074f7cb166d4c4db9a8f16
amitberko <amit.berko10@gmail.com>
2024-08-02 21:32:52 +0300

    Added calculator buttons

--- frontend/src/components/LevelCreator/CalculatorButton.jsx
+++ frontend/src/components/LevelCreator/CalculatorButton.jsx
@@ -1,17 +1,58 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-function CalculatorButton({ text, type, index }) {
+function CalculatorButton({ text, type, index, handleOpen, handleChildOpen, preview = false }) {
 	const buttonRef = useRef(null)
 	const textRef = useRef(null)
 
-	const getButtonClass = () => {
-		if (index === 8) {
-			return 'clear-button'
-		}
+	let handleClick = null
+	let buttonClass = null
+
+	const setButtonSettings = () => {
 		switch (type) {
+			case 'clear':
+				if (!preview) handleClick = handleClearButton
+				buttonClass = 'clear-button'
+				break
+			case 'insert':
+				if (!preview) handleClick = handleInsertButton
+				buttonClass = 'insert-button'
+				break
+			case 'result-changer':
+				if (!preview) handleClick = handleModifierButton
+				buttonClass = 'result-changer-button'
+				break
+			case 'order-changer':
+				buttonClass = 'order-changer-button'
+				break
+			case 'operator':
+				if (!preview) handleClick = handleOperatorButton
+				buttonClass = 'operator-button'
+				break
+
 			default:
-				return 'empty-button'
+				if (!preview) handleClick = handleEmptyButton
+				buttonClass = 'empty-button'
 		}
+	}
+
+	const handleEmptyButton = () => {
+    handleOpen()
+	}
+
+	const handleClearButton = () => {
+    console.log('clear')
+	}
+
+	const handleInsertButton = () => {
+    console.log('insert')
+	}
+
+	const handleModifierButton = () => {
+    console.log('modifier')
+	}
+
+	const handleOperatorButton = () => {
+    console.log('operator')
 	}
 
 	useEffect(() => {
@@ -24,11 +65,11 @@ function CalculatorButton({ text, type, index }) {
 			const buttonWidth = button.offsetWidth
 			const buttonHeight = button.offsetHeight
 
-			let fontSize = 40 // Start with a large font size
+			let fontSize = 35 // Start with a large font size
 			const letters = button.textContent.length
 			textElement.style.fontSize = `${fontSize}px`
 
-			// Reduce font size until text fits within button
+			// Reduce font size until text fits the button's height & width
 			while (
 				textElement.offsetWidth > buttonWidth * (0.5 + Math.min(0.3, letters * 0.1)) ||
 				(textElement.offsetHeight > buttonHeight * (0.5 + Math.min(0.3, letters * 0.1)) &&
@@ -47,12 +88,20 @@ function CalculatorButton({ text, type, index }) {
 		}
 	}, [text]) // Re-run effect if text changes
 
+	setButtonSettings()
+
+  if (preview) {
+    handleClick = () => handleChildOpen(`${text} and ${type}`)
+  }
+
 	return (
-		<button ref={buttonRef} className={`calculator-button ${getButtonClass()}`}>
-			<span ref={textRef} style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>
-        {text}
-			</span>
-		</button>
+		<>
+			<button ref={buttonRef} onClick={handleClick} className={`calculator-button ${buttonClass}`}>
+				<span ref={textRef} style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>
+					{text}
+				</span>
+			</button>
+		</>
 	)
 }
 
--- frontend/src/components/LevelCreator/LevelCreator.jsx
+++ frontend/src/components/LevelCreator/LevelCreator.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
 import { Grid } from '@mui/material'
 import CalculatorButton from './CalculatorButton'
+import TypesModal from './TypesModal'
 
 function LevelCreator() {
+	const [isOpen, setIsOpen] = useState(false)
+	const handleOpen = () => setIsOpen(true)
+	const handleClose = () => setIsOpen(false)
+
 	const [currentButtons, setCurrentButtons] = useState([
-		'+2',
-		'/5',
-		'btn3',
-		'btn4',
-		'btn5',
-		'btn6',
-		'btn7',
-		'btn8',
-		'CLEAR',
+		{ type: 'insert', text: '12' },
+		{ type: 'operator', text: '+12' },
+		{ type: 'unique', text: 'Unique' },
+		{ type: '', text: '' },
+		{ type: '', text: '' },
+		{ type: '', text: '' },
+		{ type: '', text: '' },
+		{ type: '', text: '' },
 	])
 	return (
 		<div className="calculator-container">
@@ -31,11 +35,22 @@ function LevelCreator() {
 				{currentButtons.map((button, index) => {
 					return (
 						<Grid item xs={4} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
-              <CalculatorButton text={button} index={index} />
+							<CalculatorButton
+								text={button.text}
+								handleOpen={handleOpen}
+								type={button.type}
+								index={index}
+							/>
 						</Grid>
 					)
 				})}
+
+				{/* Static button */}
+				<Grid item xs={4}>
+					<CalculatorButton type="clear" text="CLEAR" />
+				</Grid>
 			</Grid>
+			<TypesModal isOpen={isOpen} handleClose={handleClose} />
 		</div>
 	)
 }
