497780c5e025ddd56bc0f99256b1f60d910c4817
amitberko <amit.berko10@gmail.com>
2024-08-06 23:20:20 +0300

    Added the `Sort` button

---
+++ frontend/src/components/LevelCreator/ButtonPreviews/SortType.jsx
@@ -0,0 +1,42 @@
+import { Box, MenuItem, TextField } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useLevelCreator } from '../LevelCreatorProvider'
+
+function SortType() {
+	const [sortMode, setSortMode] = useState('')
+	const { setNewButton } = useLevelCreator()
+
+	useEffect(() => {
+		const text = 'Sort' + (sortMode === 'Ascending' ? '>' : '<')
+		setNewButton({ type: { color: 'order-changer', purpose: 'sort' }, text, buttonData: { sortMode } })
+	}, [sortMode])
+
+	return (
+		<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+			<TextField
+				select
+				value={sortMode}
+				onChange={(e) => setSortMode(e.target.value)}
+				fullWidth
+				variant="standard"
+				label="Sort Mode"
+				color="secondary"
+				sx={{
+					maxWidth: '200px',
+					'& .MuiSelect-select': {
+						fontSize: '1.4rem',
+						display: 'flex',
+						alignItems: 'center',
+						paddingTop: 0,
+						paddingBottom: '4px',
+					},
+				}}
+			>
+				<MenuItem value={'Ascending'}>Ascending</MenuItem>
+				<MenuItem value={'Descending'}>Descending</MenuItem>
+			</TextField>
+		</Box>
+	)
+}
+
+export default SortType
