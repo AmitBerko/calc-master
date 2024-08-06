643b135c56886d3633243805a896536e7f0e331e
amitberko <amit.berko10@gmail.com>
2024-08-06 20:35:29 +0300

    Added the `calculator` component

---
+++ frontend/src/components/LevelCreator/Calculator.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Grid } from '@mui/material'
+import CalculatorButton from './CalculatorButton'
+
+function Calculator({ currentButtons, result }) {
+	return (
+		<div className="calculator-container">
+			<div className="screen-container">
+				<div className="screen-content">
+					<div className="level-info">
+						<div className="moves">MOVES: 4</div>
+						<div className="goal">GOAL: 1234</div>
+					</div>
+					<div className="result-container">
+						<div className="result">{result}</div>
+					</div>
+				</div>
+			</div>
+			<Grid container columnSpacing={2} rowSpacing={4.5} className="buttons-container">
+				{currentButtons.map((button, index) => {
+					return (
+						<Grid item xs={4} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
+							<CalculatorButton
+								index={index}
+								text={button.text}
+								type={button.type}
+								buttonData={button.buttonData}
+							/>
+						</Grid>
+					)
+				})}
+
+				{/* Static button */}
+				<Grid item xs={4}>
+					<CalculatorButton type="clear" text="CLEAR" index={8} />
+				</Grid>
+			</Grid>
+		</div>
+	)
+}
+
+export default Calculator
--- frontend/src/components/LevelCreator/LevelCreator.jsx
+++ frontend/src/components/LevelCreator/LevelCreator.jsx
@@ -1,41 +1,17 @@
 import React from 'react'
-import { Grid } from '@mui/material'
-import CalculatorButton from './CalculatorButton'
 import TypesModal from './TypesModal'
 import { useLevelCreator } from './LevelCreatorProvider'
+import Calculator from './Calculator'
 
 function LevelCreator() {
-	const { currentButtons } = useLevelCreator()
+	const { currentButtons, result } = useLevelCreator()
 
 	return (
-		<div className="calculator-container">
-			<div className="screen-container">
-				<div className="screen-content">
-					<div className="level-info">
-						<div className="moves">MOVES: 4</div>
-						<div className="goal">GOAL: 1234</div>
-					</div>
-					<div className="result-container">
-						<div className="result">12345678</div>
-					</div>
-				</div>
-			</div>
-			<Grid container columnSpacing={2} rowSpacing={4.5} className="buttons-container">
-				{currentButtons.map((button, index) => {
-					return (
-						<Grid item xs={4} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
-							<CalculatorButton text={button.text} type={button.type} index={index} />
-						</Grid>
-					)
-				})}
-
-				{/* Static button */}
-				<Grid item xs={4}>
-					<CalculatorButton type="clear" text="CLEAR" index={8} />
-				</Grid>
-			</Grid>
+		<>
+			<Calculator currentButtons={currentButtons} result={result} />
+			<div style={{ position: 'absolute' }}>{JSON.stringify(currentButtons)}</div>
 			<TypesModal />
-		</div>
+		</>
 	)
 }
 
