0716b65fd932dd9500593a5bc4be5ddb42fc6a4a
amitberko <amit.berko10@gmail.com>
2024-08-16 18:16:48 +0300

    Added the `levels` api route

--- backend/routes/levels.js
+++ backend/routes/levels.js
@@ -1,12 +1,24 @@
 import express from 'express'
-import Levels from '../models/levelsModel.js'
+import Level from '../models/levelModel.js'
+import { authenticateToken } from './authRoute.js'
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
-  // console.log(req.body)
-	const { levelData } = req.body
-	console.log(levelData.originalSettings)
+router.post('/', authenticateToken, async (req, res) => {
+	const { buttons, originalSettings } = req.body
+
+	try {
+		const newLevel = new Level({
+			buttons,
+			originalSettings,
+			currentSettings: originalSettings,
+		})
+		await newLevel.save()
+		console.log(newLevel)
+		res.status(201).json({ message: 'Level saved successfuly' })
+	} catch (error) {
+		res.status(400).json({ error: error.message })
+	}
 })
 
 export default router
