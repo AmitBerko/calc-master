06f95f98af7c0e3ac0e3238dce92c846ff254376
amitberko <amit.berko10@gmail.com>
2024-08-16 18:16:36 +0300

    Added the `levelModel`

---
+++ backend/models/levelModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const ButtonSchema = new mongoose.Schema({
+	text: { type: String },
+	type: {
+		color: { type: String },
+		purpose: { type: String },
+	},
+	buttonData: { type: mongoose.Schema.Types.Mixed },
+})
+
+const SettingsSchema = new mongoose.Schema({
+	result: { type: Number, required: true },
+	goal: { type: Number, required: true },
+	moves: { type: Number, required: true },
+})
+
+const LevelSchema = new mongoose.Schema({
+	buttons: {
+		type: [ButtonSchema],
+		required: true,
+		validate: [(value) => value.length === 8, 'Buttons array must have length equal to 8.'],
+	},
+	originalSettings: { type: SettingsSchema, required: true },
+	currentSettings: { type: SettingsSchema, required: true },
+})
+
+const Level = mongoose.model('Level', LevelSchema)
+
+export default Level
