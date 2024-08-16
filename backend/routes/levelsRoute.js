2e5812776e0b30cc75300490f2e4e710ce44bce7
amitberko <amit.berko10@gmail.com>
2024-08-16 23:22:55 +0300

    Made a temporary get levels by username route

--- backend/routes/levelsRoute.js
+++ backend/routes/levelsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import Level from '../models/levelModel.js'
-// import User from '../models/userModel.js'
+import User from '../models/userModel.js'
 import { authenticateToken } from './authRoute.js'
 
 const router = express.Router()
@@ -15,10 +15,31 @@ router.post('/', authenticateToken, async (req, res) => {
 			currentSettings: originalSettings,
 		})
 		await newLevel.save()
-		console.log(newLevel)
+		const levelId = newLevel._id
+		const userId = req.user._id
+		await User.findByIdAndUpdate(userId, { $push: { levels: levelId } }, { new: true })
+
 		res.status(201).json({ message: 'Level saved successfuly' })
 	} catch (error) {
 		res.status(400).json({ error: error.message })
+	}
+})
+
+router.get('/:username', async (req, res) => {
+	const { username } = req.params
+	console.log('username is', username)
+	try {
+		const user = await User.findOne({ username })
+		console.log('user is', user)
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' })
+		}
+
+		const levels = await Level.find({ _id: { $in: user.levels } })
+		res.status(200).json(levels)
+		console.log('levels are', levels)
+	} catch (error) {
+		//
 	}
 })
 
