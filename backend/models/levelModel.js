3ddc6064695aceee2ad0d8a3dc72a659b49feb24
amitberko <amit.berko10@gmail.com>
2024-08-22 20:47:56 +0300

    Updated `text` type to include objects (for the transform button)

--- backend/models/levelModel.js
+++ backend/models/levelModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const ButtonSchema = new mongoose.Schema({
-	text: { type: String },
+	text: { type: mongoose.Schema.Types.Mixed },
   color: { type: String },
   type: { type: String },
 	buttonData: { type: mongoose.Schema.Types.Mixed },
