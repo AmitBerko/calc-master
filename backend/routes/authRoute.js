56b4aee0e70caf175bbab9e029f0e9b88c015708
amitberko <amit.berko10@gmail.com>
2024-08-24 19:55:41 +0300

    Fixed a typo

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import User from '../models/UserModel.js'
+import User from '../models/userModel.js'
 import bcrypt from 'bcrypt'
 
 const router = express.Router()
