8c41cdf443b503b5d843d7bb8752c3e03026cb95
amitberko <amit.berko10@gmail.com>
2024-08-28 18:59:45 +0300

    Organized code and improved textfield's ui

--- frontend/src/components/LevelExplorer.jsx
+++ frontend/src/components/LevelExplorer.jsx
@@ -1,288 +1,288 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
 import api from '../axios'
 import { useAuth } from './AuthProvider'
 import LevelCard from './LevelCard'
-import { useNavigate } from 'react-router-dom'
+import sadCalculatorImage from '../assets/sad-robot.png'
 import {
 	Box,
 	Button,
 	CircularProgress,
 	Grid,
+	InputBase,
 	Tab,
 	Tabs,
 	TextField,
 	Tooltip,
 	Typography,
 } from '@mui/material'
-import useDebounce from '../hooks/useDebounce'
-import sadCalculatorImage from '../assets/sad-robot.png'
+import {Search} from '@mui/icons-material'
 
 function LevelExplorer({ setSelectedComponent }) {
 	const [levels, setLevels] = useState(null)
 	const { user } = useAuth()
-	const [areLevelsLoading, setAreLevelsLoading] = useState(!!user)
+	const [isLoading, setIsLoading] = useState(true)
 	const [activeTab, setActiveTab] = useState(user ? 'myLevels' : 'searchLevels')
-	const [search, setSearch] = useState('')
-	const [abortController, setAbortController] = useState(null)
-	const debouncedSearch = useDebounce(search, 750)
-
+	const [usernameFilter, setUsernameFilter] = useState('')
 	const navigate = useNavigate()
 
-  useEffect(() => {
-    // Preload the image
-    const img = new Image()
-    img.src = sadCalculatorImage
-  }, [])
+	useEffect(() => {
+		const img = new Image()
+		img.src = sadCalculatorImage
+	}, [])
 
 	useEffect(() => {
-		const fetchLevels = async () => {
-			if (abortController) {
-				abortController.abort()
-			}
-
-			const controller = new AbortController()
-			setAbortController(controller)
-
-			try {
-				setAreLevelsLoading(true)
-				let response
-				if (activeTab === 'myLevels') {
-					response = await api.get('/levels/me', { signal: controller.signal })
-				} else if (activeTab === 'searchLevels') {
-					response = await api.get('/levels', { signal: controller.signal })
-				}
-				setLevels(response.data)
-				setAreLevelsLoading(false)
-			} catch (error) {
-				console.log(error)
-				if (error.name !== 'CanceledError') {
-					setAreLevelsLoading(false)
-				}
-			}
-		}
-
 		fetchLevels()
-	}, [activeTab])
+	}, [activeTab, user])
 
-	useEffect(() => {
-		const fetchLevels = async () => {
-			try {
-				let response
-				if (activeTab === 'searchLevels') {
-					setAreLevelsLoading(true)
-					response = await api.get(`/levels/searchUsername/${debouncedSearch}`)
-					setLevels(response.data) // This should be inside the if block
-				}
-			} catch (error) {
-				console.log(error)
-			} finally {
-				setAreLevelsLoading(false)
-			}
+	const fetchLevels = async () => {
+		try {
+			setIsLoading(true)
+			const endpoint = activeTab === 'myLevels' ? '/levels/me' : '/levels'
+			const response = await api.get(endpoint)
+			setLevels(response.data)
+		} catch (error) {
+			console.error('Error fetching levels:', error)
+		} finally {
+			setIsLoading(false)
 		}
+	}
 
-		if (debouncedSearch.length <= 2) return
-		fetchLevels()
-	}, [debouncedSearch])
+	const filteredLevels = levels
+		? levels.filter((level) =>
+				level.creatorName.toLowerCase().startsWith(usernameFilter.toLowerCase())
+		  )
+		: []
 
-	function handleLevelSelect(levelId, levelData) {
-		// Navigate to the shareable level link, and send the levelData
+	const handleLevelSelect = (levelId, levelData) => {
 		navigate(`/play/${levelId}`, { state: { selectedLevelData: levelData } })
 	}
 
-	async function onDelete(levelId) {
+	const handleDelete = async (levelId) => {
 		try {
 			await api.delete(`/levels/${levelId}`)
-			setLevels((prevLevels) => {
-				const filteredLevels = prevLevels.filter((level) => level._id !== levelId)
-				return filteredLevels
-			})
+			setLevels((prevLevels) => prevLevels.filter((level) => level._id !== levelId))
 		} catch (error) {
-			console.log(error)
+			console.error('Error deleting level:', error)
 		}
 	}
 
-	let componentToRender = null
-
-	if (areLevelsLoading) {
-		componentToRender = (
-			<Box
-				sx={{
-					display: 'flex',
-					justifyContent: 'center',
-					height: '20vh',
-					alignItems: 'center',
-				}}
-			>
-				<CircularProgress color="secondary" thickness={5} size={'calc(3vw + 4rem)'} />
-			</Box>
-		)
-	} else if (levels?.length === 0) {
-		componentToRender = (
-			<Box sx={{ display: 'flex', justifyContent: 'center', px: { xs: 2, sm: 1 } }}>
-				<Box
-					sx={{
-						display: 'inline-flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						flexDirection: 'column',
-						px: { xs: '1rem', sm: '2rem' },
-						py: '2.25rem',
-						backgroundColor: 'rgb(30, 30, 30)',
-						borderRadius: '1rem',
-						boxShadow: '0 4px 20px rgba(0, 0, 0, 0.3)',
-					}}
-				>
-					<img
-						width={'90%'}
-						style={{
-							borderRadius: '1rem',
-							marginBottom: '1rem',
-							boxShadow: '0 0 15px rgba(255, 255, 255, 0.1)',
-							maxWidth: '350px',
-						}}
-						src={sadCalculatorImage}
-						alt="Sad Calculator"
-					/>
-					<Typography
-						variant="h4"
-						align="center"
-						sx={{
-							fontWeight: 'bold',
-							marginBottom: { xs: '0.5rem' },
-							textShadow: '0 2px 4px rgba(0, 0, 0, 0.5)',
-						}}
-					>
-						No Levels Found
-					</Typography>
-
-					<Typography
-						variant="body1"
-						align="center"
-						sx={{
-							color: '#bbb',
-							maxWidth: '80%',
-							marginBottom: activeTab === 'myLevels' ? '1rem' : '0',
-						}}
-					>
-						{activeTab === 'myLevels'
-							? 'Design your first challenge here:'
-							: 'Make sure the username is spelled correctly'}
-					</Typography>
-
-					{activeTab === 'myLevels' && (
-						<Button
-							variant="contained"
-							color="success"
-							sx={{
-								padding: '10px 20px',
-								fontSize: '1rem',
-								color: 'rgba(255, 255, 255, 0.85)',
-							}}
-							onClick={() => setSelectedComponent('levelCreator')}
-						>
-							Create a Level
-						</Button>
-					)}
-				</Box>
-			</Box>
-		)
-	} else if (levels) {
-		componentToRender = (
-			<>
-				<Box sx={{ p: { xs: 0, sm: 2 } }}>
-					<Grid container spacing={4} justifyContent="center">
-						{levels.map((level) => (
-							<Grid item key={level._id} xs={12} md={6} lg={4}>
-								<Box
-									sx={{
-										display: 'flex',
-										justifyContent: 'center',
-									}}
-								>
-									<LevelCard
-										onPlay={() => handleLevelSelect(level._id, level)}
-										createdAt={level.createdAt}
-										creatorName={level.creatorName}
-										originalSettings={level.originalSettings}
-										isMyLevel={user?.username === level.creatorName}
-										onDelete={() => onDelete(level._id)}
-									/>
-								</Box>
-							</Grid>
-						))}
-					</Grid>
-				</Box>
-			</>
-		)
+	const handleTabChange = () => {
+		setLevels(null)
+		setIsLoading(true)
 	}
 
-	const handleTabChange = (event, newValue) => {
-		setLevels(null)
-		setActiveTab(newValue)
-		setSearch('')
+	const renderContent = () => {
+		if (isLoading) {
+			return <LoadingSpinner />
+		}
+		if (!filteredLevels.length) {
+			return <NoLevelsFound activeTab={activeTab} setSelectedComponent={setSelectedComponent} />
+		}
+		return (
+			<LevelGrid
+				levels={filteredLevels}
+				user={user}
+				onPlay={handleLevelSelect}
+				onDelete={handleDelete}
+			/>
+		)
 	}
 
 	return (
-		<>
-			<Box sx={{ py: 3, px: { xs: 2, sm: 3 } }}>
-				<Tabs
-					value={activeTab}
-					onChange={handleTabChange}
-					sx={{
-						marginBottom: '1.5rem',
-						'& .MuiTabs-scroller': { display: 'flex', justifyContent: 'center' },
-					}}
-				>
-					<Tab
-						label={
-							<Tooltip
-								leaveTouchDelay={2250}
-								title={user ? '' : 'Sorry! You must be logged in'}
-								arrow
-								enterTouchDelay={20}
-								PopperProps={{
-									modifiers: [
-										{
-											name: 'offset',
-											options: {
-												offset: [0, 6], // Move the tooltip down a bit
-											},
-										},
-									],
-									sx: {
-										'& .MuiTooltip-tooltip': {
-											marginTop: '0px !important', // Remove any default margin
-										},
-									},
-								}}
-							>
-								<span style={{ padding: '12px 16px', pointerEvents: 'auto' }}>My Levels</span>
-							</Tooltip>
-						}
-						disabled={!user}
-						sx={{ p: 0 }}
-						value="myLevels"
-					/>
-					<Tab label="Search Levels" value="searchLevels" />
-				</Tabs>
-				{activeTab === 'searchLevels' && (
-					<>
-						<Box sx={{ display: 'flex', justifyContent: 'center' }}>
-							<TextField
-								sx={{ marginBottom: { xs: 2, sm: 3 }, width: '15rem' }}
-								color="secondary"
-								variant="standard"
-								label="Filter By Username"
-								value={search}
-								onChange={(e) => setSearch(e.target.value)}
-							/>
-						</Box>
-					</>
-				)}
-				{componentToRender}
-			</Box>
-		</>
+		<Box sx={{ py: 3, px: { xs: 2, sm: 3 } }}>
+			<TabSelector
+				activeTab={activeTab}
+				setActiveTab={setActiveTab}
+				user={user}
+				handleTabChange={handleTabChange}
+			/>
+			{activeTab === 'searchLevels' && (
+				<UsernameFilter value={usernameFilter} onChange={setUsernameFilter} />
+			)}
+			{renderContent()}
+		</Box>
 	)
 }
+
+const LoadingSpinner = () => (
+	<Box sx={{ display: 'flex', justifyContent: 'center', height: '20vh', alignItems: 'center' }}>
+		<CircularProgress color="secondary" thickness={5} size={'calc(3vw + 4rem)'} />
+	</Box>
+)
+
+const NoLevelsFound = ({ activeTab, setSelectedComponent }) => (
+	<Box sx={{ display: 'flex', justifyContent: 'center', px: { xs: 2, sm: 1 } }}>
+		<Box
+			sx={{
+				display: 'inline-flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flexDirection: 'column',
+				px: { xs: '1rem', sm: '2rem' },
+				py: '2.25rem',
+				backgroundColor: 'rgb(30, 30, 30)',
+				borderRadius: '1rem',
+				boxShadow: '0 4px 20px rgba(0, 0, 0, 0.3)',
+			}}
+		>
+			<img
+				width={'90%'}
+				style={{
+					borderRadius: '1rem',
+					marginBottom: '1rem',
+					boxShadow: '0 0 15px rgba(255, 255, 255, 0.1)',
+					maxWidth: '350px',
+				}}
+				src={sadCalculatorImage}
+				alt="Sad Calculator"
+			/>
+			<Typography
+				variant="h4"
+				align="center"
+				sx={{
+					fontWeight: 'bold',
+					marginBottom: { xs: '0.5rem' },
+					textShadow: '0 2px 4px rgba(0, 0, 0, 0.5)',
+				}}
+			>
+				No Levels Found
+			</Typography>
+			<Typography
+				variant="body1"
+				align="center"
+				sx={{
+					color: '#bbb',
+					maxWidth: '80%',
+					marginBottom: activeTab === 'myLevels' ? '1rem' : '0',
+				}}
+			>
+				{activeTab === 'myLevels'
+					? 'Design your first challenge here:'
+					: 'Make sure the username is spelled correctly'}
+			</Typography>
+			{activeTab === 'myLevels' && (
+				<Button
+					variant="contained"
+					color="success"
+					sx={{
+						padding: '10px 20px',
+						fontSize: '1rem',
+						color: 'rgba(255, 255, 255, 0.85)',
+					}}
+					onClick={() => setSelectedComponent('levelCreator')}
+				>
+					Create a Level
+				</Button>
+			)}
+		</Box>
+	</Box>
+)
+
+const LevelGrid = ({ levels, user, onPlay, onDelete }) => (
+	<Box sx={{ p: { xs: 0, sm: 2 } }}>
+		<Grid container spacing={4} justifyContent="center">
+			{levels.map((level) => (
+				<Grid item key={level._id} xs={12} md={6} lg={4}>
+					<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+						<LevelCard
+							onPlay={() => onPlay(level._id, level)}
+							createdAt={level.createdAt}
+							creatorName={level.creatorName}
+							originalSettings={level.originalSettings}
+							isMyLevel={user?.username === level.creatorName}
+							onDelete={() => onDelete(level._id)}
+						/>
+					</Box>
+				</Grid>
+			))}
+		</Grid>
+	</Box>
+)
+
+const TabSelector = ({ activeTab, setActiveTab, user, handleTabChange }) => (
+	<Tabs
+		value={activeTab}
+		onChange={(_, newValue) => {
+			setActiveTab(newValue)
+      handleTabChange()
+		}}
+		sx={{
+			marginBottom: '1.5rem',
+			'& .MuiTabs-scroller': { display: 'flex', justifyContent: 'center' },
+		}}
+	>
+		<Tab
+			label={
+				<Tooltip
+					leaveTouchDelay={2250}
+					title={user ? '' : 'Sorry! You must be logged in'}
+					arrow
+					enterTouchDelay={20}
+					PopperProps={{
+						modifiers: [
+							{
+								name: 'offset',
+								options: {
+									offset: [0, 6],
+								},
+							},
+						],
+						sx: {
+							'& .MuiTooltip-tooltip': {
+								marginTop: '0px !important',
+							},
+						},
+					}}
+				>
+					<span style={{ padding: '12px 16px', pointerEvents: 'auto' }}>My Levels</span>
+				</Tooltip>
+			}
+			disabled={!user}
+			sx={{ p: 0 }}
+			value="myLevels"
+		/>
+		<Tab label="Search Levels" value="searchLevels" />
+	</Tabs>
+)
+
+const UsernameFilter = ({ value, onChange }) => (
+	<Box
+		sx={{
+			display: 'flex',
+      justifyContent: 'center',
+			mb: 3,
+		}}
+	>
+
+		<Box
+			sx={{
+				display: 'flex',
+				alignItems: 'center',
+				width: '100%',
+				maxWidth: '20rem',
+				backgroundColor: 'rgb(70, 70, 70)',
+				borderRadius: '1.15rem',
+				padding: '4px 12px',
+				boxShadow: '0 2px 8px rgba(0, 0, 0, 0.15)',
+			}}
+		>
+			<Search sx={{ color: 'text.secondary', mr: 1 }} />
+			<InputBase
+				sx={{
+					ml: 1,
+					width: '100%',
+					color: 'text.primary',
+					'& input': {
+						padding: '8px 0',
+					},
+				}}
+				placeholder="Enter username..."
+				value={value}
+				onChange={(e) => onChange(e.target.value)}
+			/>
+		</Box>
+	</Box>
+)
 
 export default LevelExplorer
